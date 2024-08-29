de727e506e546851d7a2eddff3d7468bfd00ba5d
Fiorella <U202120418@upc.edu.pe>
2024-08-29 18:44:09 -0500

    Initial commit

---
+++ src/main/java/com/acme/web/services/webservicesmain/WebServicesMainApplication.java
@@ -0,0 +1,13 @@
+package com.acme.web.services.webservicesmain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebServicesMainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebServicesMainApplication.class, args);
+    }
+
+}
