8159816ca3de46b69ed15e07b4b53685e550337a
Giacomo202210029 <u202210029@upc.edu.pe>
2024-08-31 23:58:43 -0500

    feat(management): Added Infrastructure Layer

---
+++ src/main/java/com/acme/web/services/management/infrastructure/persistence/jpa/repositories/CageRepository.java
@@ -0,0 +1,16 @@
+package com.acme.web.services.management.infrastructure.persistence.jpa.repositories;
+
+import com.acme.web.services.management.domain.model.aggregates.Cage;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+/**
+ * JPA Repository for Cage entity.
+ * It extends JpaRepository to have access to CRUD operations.
+ * It contains the method findAllByBreederId to find all cages by breeder id.
+ * @author Giacomo Zoppi Rodriguez - u2022210029 * @version 1.0
+ */
+public interface CageRepository extends JpaRepository<Cage, Long> {
+    List<Cage> findAllByBreederId(Long breederId);
+}
---
+++ src/main/java/com/acme/web/services/management/infrastructure/persistence/jpa/repositories/ExpenseRepository.java
@@ -0,0 +1,17 @@
+package com.acme.web.services.management.infrastructure.persistence.jpa.repositories;
+
+import com.acme.web.services.management.domain.model.aggregates.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+/**
+ * JPA Repository for Expense entity.
+ * It extends JpaRepository to get access to CRUD operations.
+ * it contains a method to find all expenses by breeder id.
+ * @author Giacomo Zoppi Rodriguez - u2022210029 */
+@Repository
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {
+    List<Expense> findAllByBreederId(Long breederId);
+}
